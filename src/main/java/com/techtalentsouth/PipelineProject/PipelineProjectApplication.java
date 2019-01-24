4586413f623c5e094a82f3b5f6894aab0b749d76
Kellie Chinchar <kelliechinchar@Kellies-MacBook-Pro.local>
2019-01-24 09:04:43 -0500

    initial commit

---
+++ src/main/java/com/techtalentsouth/PipelineProject/PipelineProjectApplication.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.PipelineProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineProjectApplication.class, args);
+	}
+
+}
+
